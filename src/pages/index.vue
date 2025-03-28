76a08f922457d4576596ca5a0ae598fc417fb89c
hmtuan011103 <hoangmanhtuan0111@gmail.com>
2025-03-28 15:16:17 +0700

    Bootstrap the App Backend with Supabase

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
     <div>
         <h1>Home Page</h1>
-        <!-- <RouterLink to="/projects">Go to projects</RouterLink> -->
         <RouterLink 
             :to="{ name: '/projects/[id]', params: { id: 1 } }"
         >
