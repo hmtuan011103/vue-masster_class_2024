811f52ad890251c5982bfdcdc0b8488f3e3894c4
hmtuan011103 <hoangmanhtuan0111@gmail.com>
2025-03-31 15:28:20 +0700

    shorthand v-slot to #

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 
 <template>
   <AuthLayout>
-    <RouterView v-slot="{ Component, route }">
+    <RouterView #default="{ Component, route }">
       <Suspense v-if="Component" :timeout="0">
         <Component :is="Component" :key="route.name"></Component>
 
