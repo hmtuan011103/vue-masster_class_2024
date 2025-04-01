41a2100462bff18e0ca0296ec0fa49510c9ef50e
hmtuan011103 <hoangmanhtuan0111@gmail.com>
2025-04-01 11:03:08 +0700

    done refactoring the project Architecture for Scalability

--- src/utils/supaQueries.ts
+++ src/utils/supaQueries.ts
@@ -10,9 +10,13 @@ export const tasksWithProjectsQuery = supabase.from('tasks').select(`
 export type TasksWithProjects = QueryData<typeof tasksWithProjectsQuery>
 
 export const projectsQuery = supabase.from('projects').select()
-export type Projects= QueryData<typeof projectsQuery>
+export type Projects = QueryData<typeof projectsQuery>
 
-export const projectQuery = (slug: string) => supabase.from('projects').select(`
+export const projectQuery = (slug: string) =>
+  supabase
+    .from('projects')
+    .select(
+      `
     *,
     tasks (
       id,
@@ -20,8 +24,27 @@ export const projectQuery = (slug: string) => supabase.from('projects').select(`
       status,
       due_date
     )
-  `)
-  .eq('slug', slug)
-  .single()
-export type Project= QueryData<ReturnType <typeof projectQuery>>
+  `,
+    )
+    .eq('slug', slug)
+    .single()
+export type Project = QueryData<ReturnType<typeof projectQuery>>
 
+export const taskQuery = (id: string) => {
+  return supabase
+    .from('tasks')
+    .select(
+      `
+    *,
+    projects (
+      id,
+      name,
+      slug
+    )
+  `,
+    )
+    .eq('id', id)
+    .single()
+}
+
+export type Task = QueryData<ReturnType<typeof taskQuery>>
