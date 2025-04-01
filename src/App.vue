f26084c084138ed2150fbe669be7dfdf5e27c71f
hmtuan011103 <hoangmanhtuan0111@gmail.com>
2025-04-01 15:57:32 +0700

    error page in frontend

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
+import { useErrorStore } from './stores/error';
+
+const { activeError } = storeToRefs(useErrorStore())
 </script>
 
 <template>
   <AuthLayout>
-    <RouterView #default="{ Component, route }">
+    <AppErrorPage v-if="activeError" />
+
+    <RouterView v-else #default="{ Component, route }">
       <Suspense v-if="Component" :timeout="0">
         <Component :is="Component" :key="route.name"></Component>
 
