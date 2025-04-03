02227d32dbf3adfa5c627e283870a37ac65e6df2
hmtuan011103 <hoangmanhtuan0111@gmail.com>
2025-04-03 11:46:38 +0700

    router guard

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
-import { supabase } from './lib/supabaseClient';
-import { useAuthStore } from './stores/auth';
 import { useErrorStore } from './stores/error';
 
 const errorStore = useErrorStore()
-const authStore = useAuthStore()
 
 const { activeError } = storeToRefs(useErrorStore())
 
@@ -12,10 +9,7 @@ onErrorCaptured((error) => {
   errorStore.setError({ error })
 })
 
-onMounted(async () => {
-  const { data } = await supabase.auth.getSession()
-  if (data.session?.user) await authStore.setAuth(data.session)
-})
+onMounted(async () => {})
 </script>
 
 <template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,32 +1,15 @@
-// import { h } from 'vue'
+import { useAuthStore } from '@/stores/auth'
 import { createRouter, createWebHistory } from 'vue-router/auto'
 import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
-  // routes: [
-  //   {
-  //     path: '/',
-  //     name: 'home',
-  //     component: () => import('@/views/HomeView.vue')
-  //   },
-  //   {
-  //     path: '/projects',
-  //     name: 'projects',
-  //     component: () => import('@/views/ProjectsView.vue')
-  //   },
-  //   {
-  //     path: '/projects/:id',
-  //     name: 'single-project',
-  //     component: () => import('@/views/SingleProjectView.vue')
-  //   },
-  //   {
-  //     path: '/:catchAll(.*)*',
-  //     name: 'NotFound',
-  //     component: h('p', { style: 'color: red'}, '404 Not Found')
-  //   }
-  // ]
+})
+
+router.beforeEach(async () => {
+  const { getSession } = useAuthStore()
+  await getSession()
 })
 
 export default router
