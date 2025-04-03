15aa46e1e348c66a22b0566830eb67f3345368e5
hmtuan011103 <hoangmanhtuan0111@gmail.com>
2025-04-03 11:40:25 +0700

    auth session

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
+import { supabase } from './lib/supabaseClient';
+import { useAuthStore } from './stores/auth';
 import { useErrorStore } from './stores/error';
 
 const errorStore = useErrorStore()
+const authStore = useAuthStore()
 
 const { activeError } = storeToRefs(useErrorStore())
 
 onErrorCaptured((error) => {
   errorStore.setError({ error })
+})
+
+onMounted(async () => {
+  const { data } = await supabase.auth.getSession()
+  if (data.session?.user) await authStore.setAuth(data.session)
 })
 </script>
 
--- src/stores/error.ts
+++ src/stores/error.ts
@@ -1,13 +1,18 @@
-import type { CustomError, ExtendedPostgrestError } from "@/types/Error";
-import type { PostgrestError } from "@supabase/supabase-js";
+import type { CustomError, ExtendedPostgrestError } from '@/types/Error'
+import type { PostgrestError } from '@supabase/supabase-js'
 
 export const useErrorStore = defineStore('error-store', () => {
   const activeError = ref<null | CustomError | ExtendedPostgrestError>(null)
   const isCustomError = ref(false)
 
-  const setError = ({ error, customCode }: { error: string | PostgrestError | Error; customCode?: number }) => {
-
-    if ( typeof error === 'string') isCustomError.value = true
+  const setError = ({
+    error,
+    customCode,
+  }: {
+    error: string | PostgrestError | Error
+    customCode?: number
+  }) => {
+    if (typeof error === 'string') isCustomError.value = true
 
     if (typeof error === 'string' || error instanceof Error) {
       activeError.value = typeof error === 'string' ? Error(error) : error
@@ -28,6 +33,10 @@ export const useErrorStore = defineStore('error-store', () => {
     activeError,
     setError,
     isCustomError,
-    clearError
+    clearError,
   }
 })
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useErrorStore, import.meta.hot))
+}
